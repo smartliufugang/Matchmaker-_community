519acd9324d7820448c9460997f5ebf64d274db8
liufugang <15753936809@163.com>
2020-01-16 17:56:46 +0800

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-     
-    </div>
+   <el-container>
+     <el-header class="el-header"> 
+       <navT></navT>
+     </el-header>
+     <el-main>
+       <div class="container">
+
+       </div>
+     </el-main>
+     <el-footer>
+       <p style="width:100%;height:200px;background:yellow"></p>
+     </el-footer>
+   </el-container>
    
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import navT from '@/components/nav'
+export default Vue.extend({
+  components:{
+    navT
+  }
+})
+</script>
 
 <style lang="scss">
+  body{
+    padding:0;
+    margin:0;
+    background:#f9f7ae;
+    background-image: url('../src/assets/love.gif') ;
+    .el-header{
+      margin: 0 auto;
+      width: 75%;
+      background:none;
+    }
+    .container{
+      margin: 0 auto;
+      background: red;
+      width: 75%;
+      height: 1000px;
+      opacity: 0.3;
+    }
+  }
+  
 
 </style>
---
+++ src/components/nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-menu  class='nav' mode="horizontal">
+        <el-row  type="flex" :gutter="20">
+            <el-col  :xs="24" :sm="8" :xl="8" :md="8" :lg="8">
+                <el-menu-item class="nav-item">首页</el-menu-item>
+            </el-col>
+            <el-col  :xs="24" :sm="8" :xl="8" :md="8" :lg="8">
+        <el-menu-item class="nav-item"></el-menu-item>
+            </el-col>
+            <el-col  :xs="24" :sm="8" :xl="8" :md="8" :lg="8">
+                <el-menu-item class="nav-item"></el-menu-item>
+            </el-col>
+        </el-row>
+        
+        
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+ .nav{
+     background:blue;
+     width: 100%;
+     border: 0;
+     .nav-item{
+         color: white;
+        text-align: center;
+     }
+ }
+</style>
